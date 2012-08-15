d0a9e6541d044d53b7a9cb3e40c4b59aa0a528ab
Simon Jefford <simon.jefford@gmail.com>
2012-08-15 18:36:25 +0100

    method_missing for entry attributes

--- lib/rdayone/entry.rb
+++ lib/rdayone/entry.rb
@@ -5,14 +5,19 @@ module Rdayone
     def initialize(identifier, plist_hash = {})
       @identifier = identifier
       @plist_hash = plist_hash
+      @attribute_mappings = {
+        text: "Entry Text",
+        creation_date: "Creation Date"
+      }
     end
 
-    def text
-      @plist_hash["Entry Text"]
-    end
-
-    def creation_date
-      @plist_hash["Creation Date"]
+    def method_missing(method, *args)
+      key = @attribute_mappings[method]
+      if key
+        @plist_hash[key]
+      else
+        super
+      end
     end
   end
 end
